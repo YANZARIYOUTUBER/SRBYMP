bbfeb3917ee6d1dd9b7fde72098c113e2d922979
Yanzari <soniceduardo2023@gmail.com>
2025-08-30 21:29:32 -0300

    Add files via upload

---
+++ tools/srb2ympython/bindings.cpp
@@ -0,0 +1,13 @@
+#include <pybind11/pybind11.h>
+// any inclusion
+
+namespace py = pybind11;
+
+// Lets Add
+//void function_of_the_source_code() {
+    // Chame a função do código-fonte do SRB2 aqui
+//}
+
+//PYBIND11_MODULE(SRBYMPYTHON, m) {
+//    m.def("function", &function_of_the_source_code, "Desc");
+//}
